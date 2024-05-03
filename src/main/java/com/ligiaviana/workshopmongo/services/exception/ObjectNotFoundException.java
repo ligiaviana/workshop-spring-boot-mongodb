abc998dd909539d07f74de81c716814cebc5736a
Ligia Viana <ligia.viana.azevedo@gmail.com>
2024-05-03 11:50:21 -0300

    Getting an user by id

---
+++ src/main/java/com/ligiaviana/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.ligiaviana.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
