451652fed7b2a2e75809818dfd48f9517617cf8f
Ligia Viana <ligia.viana.azevedo@gmail.com>
2024-05-03 15:24:31 -0300

    User insertion with POST

--- src/main/java/com/ligiaviana/workshopmongo/services/UserService.java
+++ src/main/java/com/ligiaviana/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ligiaviana.workshopmongo.domain.User;
+import com.ligiaviana.workshopmongo.dto.UserDTO;
 import com.ligiaviana.workshopmongo.repository.UserRepository;
 import com.ligiaviana.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,13 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
 }
