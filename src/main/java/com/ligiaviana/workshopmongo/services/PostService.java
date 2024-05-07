45b638fff150d08749fcd3e6393288bab2922f97
Ligia Viana <ligia.viana.azevedo@gmail.com>
2024-05-07 15:18:11 -0300

    Getting a post by id

---
+++ src/main/java/com/ligiaviana/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.ligiaviana.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ligiaviana.workshopmongo.domain.Post;
+import com.ligiaviana.workshopmongo.repository.PostRepository;
+import com.ligiaviana.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
