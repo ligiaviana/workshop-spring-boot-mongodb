c87b775c155b926577fac5ee2a0d280c537f0ab1
Ligia Viana <ligia.viana.azevedo@gmail.com>
2024-05-07 16:09:57 -0300

    Comments on posts

--- src/main/java/com/ligiaviana/workshopmongo/domain/Post.java
+++ src/main/java/com/ligiaviana/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.ligiaviana.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.ligiaviana.workshopmongo.dto.AuthorDTO;
+import com.ligiaviana.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -71,6 +76,14 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -88,4 +101,5 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
+
 }
